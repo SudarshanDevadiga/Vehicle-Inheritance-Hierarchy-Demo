4c2070954d4567537e2db36cf5785a5098661524
SudarshanDevadiga <sudarshandevadiga200@gmail.com>
2025-03-10 10:57:23 +0530

    Add files via upload

---
+++ Hierachical.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Vehicle {
+    public:
+        Vehicle() {
+            cout << "This is Vehicle" << endl; 
+        }
+};
+
+class FourWheeler : public Vehicle {
+    public:
+        FourWheeler() {
+            cout << "This is Four Wheeler class" << endl;
+        }
+};
+
+class Car : public Vehicle {
+    public:
+        Car() {
+            cout << "This is Car class" << endl;
+        }
+};
+
+int main() {
+    Car obj;
+    FourWheeler obj2;
+    return 0;
+}
